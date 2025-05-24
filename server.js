9b16941d989241b819d440b67f9da47ada20cb92
Jayesh Patil <patiljayesh95666@gmail.com>
2025-05-24 22:57:15 +0530

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: ["https://aaramghar.onrender.com", "http://localhost:3000"],
+    origin: ["https://aaramghar.netlify.app", "http://localhost:3000"],
     credentials: true,
   })
 );
